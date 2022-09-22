7fef5fb0759cbbf3074e924ca01bd39e1e9294e5
Isaac <uniqucode@gmail.com>
2022-09-22 19:40:54 +0100

    Refrencing brushSlider

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ const activeToolEl = document.getElementById('active-tool');
 const brushColorBtn = document.getElementById('brush-color');
 const brushIcon = document.getElementById('brush');
 const brushSize = document.getElementById('brush-size');
+const brushSlider = document.getElementById('brush-slider');
