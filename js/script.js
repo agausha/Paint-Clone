85f4ada959ca1d42d6780ca0afd526328792dce9
Isaac <uniqucode@gmail.com>
2022-09-22 19:42:03 +0100

    Refrencing bucketColorBtn

--- js/script.js
+++ js/script.js
@@ -4,3 +4,4 @@ const brushColorBtn = document.getElementById('brush-color');
 const brushIcon = document.getElementById('brush');
 const brushSize = document.getElementById('brush-size');
 const brushSlider = document.getElementById('brush-slider');
+const bucketColorBtn = document.getElementById('bucket-color');
