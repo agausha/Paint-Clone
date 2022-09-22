ee052bb99f546b1285d00521c6da1ada888ec1e7
Isaac <uniqucode@gmail.com>
2022-09-22 19:37:14 +0100

    Refrencing brushIcon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const BRUSH_TIME = 1500;
 const activeToolEl = document.getElementById('active-tool');
 const brushColorBtn = document.getElementById('brush-color');
+const brushIcon = document.getElementById('brush');
