fb468eb7b2092988b3fed886e16162484fc3afb9
Isaac <uniqucode@gmail.com>
2022-09-22 19:38:13 +0100

    Refrencing brushSize

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const BRUSH_TIME = 1500;
 const activeToolEl = document.getElementById('active-tool');
 const brushColorBtn = document.getElementById('brush-color');
 const brushIcon = document.getElementById('brush');
+const brushSize = document.getElementById('brush-size');
